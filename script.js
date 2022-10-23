db20ba96989eb9389d2c998590b87b94263813ff
itsmeMaulana <105365445+itsmeMaulana@users.noreply.github.com>
2022-10-23 21:33:03 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+let person = window.prompt("Masukan nama ");
+if (person == null || person == "") {
+    alert("Data belum terisi");
+  } else {
+    alert("Hello " + person + " Selamat datang, jangan lupa berikan pendapat anda mengenai web ini melalui media sosial saya") ;
+  }
